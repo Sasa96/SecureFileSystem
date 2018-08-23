fbd00100cfbde231df856a0a74fffb7db4535220
kk96 <k96.kord@gmail.com>
2018-08-23 23:37:34 +0200

    RSA fixed

--- src/securefilesystem/RSA.java
+++ src/securefilesystem/RSA.java
@@ -4,32 +4,36 @@
  * and open the template in the editor.
  */
 package securefilesystem;
+
 import javax.crypto.*;
 import java.security.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
 public class RSA {
-     public static KeyPair buildKeyPair() throws NoSuchAlgorithmException {
+
+    public static KeyPair keypair;
+
+    public static void buildKeyPair() throws NoSuchAlgorithmException {
         final int keySize = 2048;
         KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-        keyPairGenerator.initialize(keySize);      
-        return keyPairGenerator.genKeyPair();
+        keyPairGenerator.initialize(keySize);
+
+        keypair = keyPairGenerator.genKeyPair();
+
     }
 
-    public static byte[] encrypt(PublicKey pubKey, String message) throws Exception {
-        Cipher cipher = Cipher.getInstance("RSA");  
-        cipher.init(Cipher.ENCRYPT_MODE, pubKey);  
+    public static byte[] encrypt(String message) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, keypair.getPublic());
 
-        return cipher.doFinal(message.getBytes());  
+        return cipher.doFinal(message.getBytes());
     }
-    
-    public static byte[] decrypt(PrivateKey privKey, byte [] encrypted) throws Exception {
-        Cipher cipher = Cipher.getInstance("RSA");  
-        cipher.init(Cipher.DECRYPT_MODE, privKey);
-        
+
+    public static byte[] decrypt(byte[] encrypted) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, keypair.getPrivate());
+
         return cipher.doFinal(encrypted);
     }
 }
-
--- src/securefilesystem/SecureFileSystem.java
+++ src/securefilesystem/SecureFileSystem.java
@@ -22,41 +22,16 @@ public class SecureFileSystem {
         String key = "Bar12345Bar12345"; // 128 bit key
         String initVector = "RandomInitVector"; // 16 bytes IV
 
-        //String encrypted = AES.encrypt(key, initVector, "Hello World");
-        //System.out.println(encrypted);
-        //System.out.println(AES.decrypt(key, initVector, encrypted));
+
+        RSA.buildKeyPair(); //called by a button in GUI
         
-        Cipher cipher;
-        Cipher cipher2;
-        try{
-            KeyPair keys = KeyPairGenerator.getInstance("RSA").generateKeyPair();
-            cipher = Cipher.getInstance("RSA");
-            
-            cipher2 = Cipher.getInstance("RSA");
-            cipher.init(Cipher.ENCRYPT_MODE, keys.getPublic()); 
-            
-            cipher2.init(Cipher.DECRYPT_MODE,keys.getPrivate());
-         
-            
-         
-        }
-        
-        catch(Exception e)
-        {
-            
-        };
-        
-     // generate public and private keys
-        KeyPair keyPair = RSA.buildKeyPair();
-        PublicKey pubKey = keyPair.getPublic();
-        PrivateKey privateKey = keyPair.getPrivate();
         
         // encrypt the message
-        byte [] encrypted = RSA.encrypt(pubKey, "This is a secret message");     
+        byte [] encrypted = RSA.encrypt("This is a secret message");     
         System.out.println(new String(encrypted));  // <<encrypted message>>
         
         // decrypt the message
-        byte[] secret = RSA.decrypt(privateKey, encrypted);                                 
+        byte[] secret = RSA.decrypt(encrypted);                                 
         System.out.println(new String(secret));     // This is a secret message
 
     }
