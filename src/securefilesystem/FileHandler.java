503fb91d015ad8d9dd78de5099d7571b37c2a94f
kk96 <k96.kord@gmail.com>
2018-08-24 01:21:42 +0200

    .

--- src/securefilesystem/FileHandler.java
+++ src/securefilesystem/FileHandler.java
@@ -5,20 +5,96 @@
  */
 package securefilesystem;
 
+import java.io.*;
+
 /**
  *
  * @author ASUS
  */
 public class FileHandler {
-    
-    
-    void Read()
-    {
-        
+
+    public static String ReadFile(String fileName) {
+
+        // The name of the file to open.
+        String extention = fileName.substring(fileName.length() - 7);
+        boolean secretFile = extention.equals(".kksasa") ? true : false;
+        // This will reference one line at a time
+        String line = null;
+        String text = "";
+
+        try {
+            // FileReader reads text files in the default encoding.
+            FileReader fileReader
+                    = new FileReader(fileName);
+
+            // Always wrap FileReader in BufferedReader.
+            BufferedReader bufferedReader
+                    = new BufferedReader(fileReader);
+
+            if (secretFile) {
+                for (int i = 1; i <= 4; i++) {
+                    bufferedReader.readLine();
+                }
+
+            }
+
+            while ((line = bufferedReader.readLine()) != null) {
+                text = text + line + "\n";
+            }
+
+            // Always close files.
+            bufferedReader.close();
+        } catch (FileNotFoundException ex) {
+            System.out.println(
+                    "file not Found Exception '"
+                    + fileName + "'");
+        } catch (IOException ex) {
+            System.out.println(
+                    "Error reading file '"
+                    + fileName + "'");
+            // Or we could just do this: 
+            // ex.printStackTrace();
+        }
+
+        return text;
     }
-    
-    void Write()
-    {
+
+    public static void WriteFile(String text, String fileName) {
         
+        // The name of the file to open.
+
+        String extention = fileName.substring(fileName.length() - 7);
+        boolean secretFile = extention.equals(".kksasa") ? true : false;
+        String header = "";
+        if (!secretFile) {
+            header = "creator ID: \n"
+                    + "creator email: \n"
+                    + "file size: \n"
+                    + "----------------------";
+        }
+        try {
+            // Assume default encoding.
+            FileWriter fileWriter
+                    = new FileWriter(fileName);
+
+            // Always wrap FileWriter in BufferedWriter.
+            BufferedWriter bufferedWriter
+                    = new BufferedWriter(fileWriter);
+
+            // Note that write() does not automatically
+            // append a newline character.
+            bufferedWriter.write(header);
+            bufferedWriter.newLine();
+            bufferedWriter.write(text);
+
+            // Always close files.
+            bufferedWriter.close();
+        } catch (IOException ex) {
+            System.out.println(
+                    "Error writing to file '"
+                    + fileName + "'");
+
+        }
+
     }
 }
--- src/securefilesystem/SecureFileSystem.java
+++ src/securefilesystem/SecureFileSystem.java
@@ -18,23 +18,20 @@ import javax.crypto.Cipher;
  */
 public class SecureFileSystem {
 
-   public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
         String key = "Bar12345Bar12345"; // 128 bit key
         String initVector = "RandomInitVector"; // 16 bytes IV
 
-
         RSA.buildKeyPair(); //called by a button in GUI
+        String text = FileHandler.ReadFile("tmp.txt");
+        byte[] encrypted = RSA.encrypt(text);
+        FileHandler.WriteFile(new String(encrypted),"encryption.kksasa");
+       
         
-        
-        // encrypt the message
-
-        byte [] encrypted = RSA.encrypt("This is a secret message");     
-
-        System.out.println(new String(encrypted));  // <<encrypted message>>
-        
-        // decrypt the message
-        byte[] secret = RSA.decrypt(encrypted);                                 
-        System.out.println(new String(secret));     // This is a secret message
+        String test_encryption = FileHandler.ReadFile("encryption.kksasa");
+        byte[] decryption = test_encryption.getBytes();
+        //byte[] secret = RSA.decrypt(decryption);
+        //FileHandler.WriteFile(new String(secret),"decryption.txt");
 
     }
 }
